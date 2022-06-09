93a0a338b0216592e4d38415faf69cdbfc36d9d3
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-09 10:25:54 +0800

    Update DemoOne.py

--- DemoOne.py
+++ DemoOne.py
@@ -23,7 +23,7 @@ def user_input_features():
     return features
 
 from PIL import Image
-image = Image.open('Iris_virginica_2.jpg')
+image = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.fs.fed.us%2Fwildflowers%2Fbeauty%2Firis%2FBlue_Flag%2Fimages%2Firis_virginica%2Firis_virginica_virginica_lg.jpg&imgrefurl=https%3A%2F%2Fwww.fs.fed.us%2Fwildflowers%2Fbeauty%2Firis%2FBlue_Flag%2Firis_virginica.shtml&tbnid=yKIaKHMxIOyAVM&vet=12ahUKEwiGpdu3qJ_4AhWz_DgGHVN-Ap4QMygBegUIARDJAQ..i&docid=lWcM6aTHXEpgAM&w=800&h=596&q=Iris%20virginica&ved=2ahUKEwiGpdu3qJ_4AhWz_DgGHVN-Ap4QMygBegUIARDJAQ'
 st.image(image, caption='Iris Virginica Species')
 
 df = user_input_features()
