261d1d7c5d0b765cad39254e80f407a8f15eb94b
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-09 09:57:26 +0800

    Update DemoOne.py

--- DemoOne.py
+++ DemoOne.py
@@ -39,7 +39,8 @@ prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
 target = ['setosa','versicolor','virginica']
-st.write(target)
+a = st.write(target)
+st.dataframe(a)
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
